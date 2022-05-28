be24955081ed4738361538d9481e7392870c6173
Евгений <pshonovskij@knu.ua>
2022-05-28 19:06:54 +0300

    1

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -6,8 +6,270 @@
 #include<chrono>
 using namespace std;
 
+enum TypeOfSorting
+{
+	Sbubble=1, Sinsertion,Sshaker
+};
+//void display(vector <int> vc,int j=-1)
+//{
+//	sf::RenderWindow wind(sf::VideoMode(800, 600), "Title");
+//	sf::Event ev;
+//	sf::RectangleShape sq;
+//	sq.setFillColor(sf::Color::White);
+//	int size = vc.size();
+//	int x = 0, y = 0, l = 0, count = 0, it = 0, check = 0;
+//	float height = 0;
+//	wind.clear();
+//		while (wind.pollEvent(ev))
+//
+//		{
+//			if (ev.type == sf::Event::Closed)
+//			{
+//				wind.close();
+//			}
+//		}
+//		count = 0, it = -1;
+//		x = 0, y = 580, height = 0;
+//		sq.setPosition(x, y);
+//		while (count != size && check != 1)
+//		{
+//			sq.setFillColor(sf::Color::White);
+//			count++;
+//			it++;
+//			height = vc[it] * 10;
+//			sq.setSize(sf::Vector2f(20.0, -height));
+//			if (it ==j)
+//			{
+//				sq.setFillColor(sf::Color::Red);
+//			}
+//			wind.draw(sq);
+//			x += 25;
+//			sq.setPosition(x, y);
+//		}
+//		if (check != 1)
+//		{
+//			wind.display();
+//			
+//		}
+//		
+//		
+//	
+//} // не робит 
+
+void shaker(vector <int> vc, int size)
+{
+	int i, j, k;
+	
+	sf::RenderWindow wind(sf::VideoMode(800, 600), "Title");
+	sf::Event ev;
+	sf::RectangleShape sq;
+	sq.setFillColor(sf::Color::White);
+	int x = 0, y = 0, l = 0, count = 0, it = 0, check = 0;
+	float height = 0;
+	while (wind.isOpen())
+	{
 
 
+		while (wind.pollEvent(ev))
+
+		{
+			if (ev.type == sf::Event::Closed)
+			{
+				wind.close();
+			}
+		}
+		count = 0, it = -1;
+		x = 0, y = 580, height = 0;
+		sq.setPosition(x, y);
+		while (count != size && check != 1)
+		{
+			count++;
+			it++;
+			height = vc[it] * 10;
+			sq.setSize(sf::Vector2f(20.0, -height));
+			wind.draw(sq);
+			x += 25;
+			sq.setPosition(x, y);
+		}
+		if (check != 1)
+		{
+			wind.display();
+			this_thread::sleep_for(chrono::seconds(1));
+		}
+		
+		while (true)
+		{
+			for (i = 0; i < size;) {
+				for (j = i + 1; j < size; j++) {
+					if (vc[j] < vc[j - 1])
+					{
+						swap(vc[j], vc[j - 1]);
+						wind.clear();
+						count = 0, it = -1;
+						x = 0, y = 580, height = 0;
+						sq.setPosition(x, y);
+
+						while (count != size && check != 1)
+						{
+							sq.setFillColor(sf::Color::White);
+							count++;
+							it++;
+							height = vc[it] * 10;
+							if (it == j)
+							{
+								sq.setFillColor(sf::Color::Red);
+							}
+							sq.setSize(sf::Vector2f(20.0, -height));
+							wind.draw(sq);
+							x += 25;
+							sq.setPosition(x, y);
+						}
+						if (check != 1)
+						{
+							wind.display();
+							this_thread::sleep_for(chrono::milliseconds(50));
+
+						}
+					}
+				}
+				size--;
+				for (k = size - 1; k > i; k--) {
+					if (vc[k] < vc[k - 1])
+					{
+						swap(vc[k], vc[k - 1]);
+						wind.clear();
+						count = 0, it = -1;
+						x = 0, y = 580, height = 0;
+						sq.setPosition(x, y);
+
+						while (count != size && check != 1)
+						{
+							sq.setFillColor(sf::Color::White);
+							count++;
+							it++;
+							height = vc[it] * 10;
+							if (it == k)
+							{
+								sq.setFillColor(sf::Color::Red);
+							}
+							sq.setSize(sf::Vector2f(20.0, -height));
+							wind.draw(sq);
+							x += 25;
+							sq.setPosition(x, y);
+						}
+						if (check != 1)
+						{
+							wind.display();
+							this_thread::sleep_for(chrono::milliseconds(50));
+
+						}
+					}
+				}
+				i++;
+			}
+			
+						
+
+					
+			check = 1;
+
+		}
+	}
+}
+void insertion(vector <int> vc, int size)
+{
+	int buff = 0; 
+	int i, j;
+
+	
+
+	sf::RenderWindow wind(sf::VideoMode(800, 600), "Title");
+	sf::Event ev;
+	sf::RectangleShape sq;
+	sq.setFillColor(sf::Color::White);
+	int x = 0, y = 0, l = 0, count = 0, it = 0, check = 0;
+	float height = 0;
+	while (wind.isOpen())
+	{
+
+
+		while (wind.pollEvent(ev))
+
+		{
+			if (ev.type == sf::Event::Closed)
+			{
+				wind.close();
+			}
+		}
+		count = 0, it = -1;
+		x = 0, y = 580, height = 0;
+		sq.setPosition(x, y);
+		while (count != size && check != 1)
+		{
+			count++;
+			it++;
+			height = vc[it] * 10;
+			sq.setSize(sf::Vector2f(20.0, -height));
+			wind.draw(sq);
+			x += 25;
+			sq.setPosition(x, y);
+		}
+		if (check != 1)
+		{
+			wind.display();
+			this_thread::sleep_for(chrono::seconds(1));
+		}
+	
+		while (true)
+		{
+			
+			for (i = 1; i < size; i++)
+			{
+				buff = vc[i];
+
+				for (j = i - 1; j >= 0 && vc[j] > buff; j--)
+				{
+					vc[j + 1] = vc[j];
+					wind.clear();
+					count = 0, it = -1;
+					x = 0, y = 580, height = 0;
+					sq.setPosition(x, y);
+
+					while (count != size && check != 1)
+					{
+						sq.setFillColor(sf::Color::White);
+						count++;
+						it++;
+						height = vc[it] * 10;
+						if (it == j)
+						{
+							sq.setFillColor(sf::Color::Red);
+						}
+						sq.setSize(sf::Vector2f(20.0, -height));
+						wind.draw(sq);
+						x += 25;
+						sq.setPosition(x, y);
+					}
+					if (check != 1)
+					{
+						wind.display();
+						this_thread::sleep_for(chrono::milliseconds(50));
+
+					}
+
+				}
+
+
+				vc[j + 1] = buff;
+			}
+						
+					
+			check = 1;
+
+		}
+	}
+	
+}
 void bubble(vector <int> vc, int size)
 {
 	sf::RenderWindow wind(sf::VideoMode(800, 600), "Title");
@@ -58,11 +320,17 @@ void bubble(vector <int> vc, int size)
 						count = 0, it = -1;
 						x = 0, y = 580, height = 0;
 						sq.setPosition(x, y);
+						
 						while (count != size && check != 1)
 						{
+							sq.setFillColor(sf::Color::White);
 							count++;
 							it++;
 							height = vc[it] * 10;
+							if (it ==j)
+							{
+								sq.setFillColor(sf::Color::Red);
+							}
 							sq.setSize(sf::Vector2f(20.0, -height));
 							wind.draw(sq);
 							x += 25;
@@ -82,24 +350,46 @@ void bubble(vector <int> vc, int size)
 	}
 }
 
-int main()
+void menu()
 {
 
-	
-	int count=0,size;
+	int count = 0, size, type;
 	cout << "Enter the size" << endl;
 	cin >> size;
 	vector <int> vc;
-	int random = rand () % 55;
+	int random = rand() % 55;
 	srand(time(NULL));
-	while (count!=size)
+	while (count != size)
 	{
 		random = rand() % 55;
 		vc.push_back(random);
-			count++;
+		count++;
 	}
-
-	bubble(vc, size);
+	
+	cout << "Chooice the sort's type:\n";
+	cout << "1.Bubble\n2.Insertion\n3.Quick\n";
+	cin >> type;
+	switch (type)
+	{
+	case Sbubble:
+		bubble(vc, size);
+		break;
+	case Sinsertion:
+		insertion(vc, size);
+		break;
+	case Sshaker:
+		shaker(vc, size);
+		break;
+	default:
+		break;
+	}
+	
+}
+int main()
+{
+	menu();
+	
+	
 
 	return 0;
 }
