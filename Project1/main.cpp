ba38150b7ceea11c9ff2d64cc680c226617970af
Евгений <pshonovskij@knu.ua>
2022-06-06 18:19:38 +0300

    Add_Speed_Changing

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,11 +4,12 @@
 #include<vector>
 #include<thread>
 #include<chrono>
+#include<fstream>
 using namespace std;
 
 enum TypeOfSorting
 {
-	Sbubble=1, Sinsertion,Sshaker
+	Sbubble = 1, Sinsertion, Sshaker, Speed = 83
 };
 //void display(vector <int> vc,int j=-1)
 //{
@@ -56,7 +57,7 @@ enum TypeOfSorting
 //	
 //} // не робит 
 
-void shaker(vector <int> vc, int size, double coeff=1)
+void shaker(vector <int> vc, int size,int speed_coeff=50, double coeff = 1 )
 {
 	int i, j, k;
 	
@@ -127,7 +128,7 @@ void shaker(vector <int> vc, int size, double coeff=1)
 						if (check != 1)
 						{
 							wind.display();
-							this_thread::sleep_for(chrono::milliseconds(50));
+							this_thread::sleep_for(chrono::milliseconds(speed_coeff));
 
 						}
 					}
@@ -176,7 +177,7 @@ void shaker(vector <int> vc, int size, double coeff=1)
 		}
 	}
 }
-void insertion(vector <int> vc, int size, double coeff = 1)
+void insertion(vector <int> vc, int size,int speed_coeff=50, double coeff = 1)
 {
 	int buff = 0; 
 	int i, j;
@@ -253,7 +254,7 @@ void insertion(vector <int> vc, int size, double coeff = 1)
 					if (check != 1)
 					{
 						wind.display();
-						this_thread::sleep_for(chrono::milliseconds(50));
+						this_thread::sleep_for(chrono::milliseconds(speed_coeff));
 
 					}
 
@@ -270,7 +271,7 @@ void insertion(vector <int> vc, int size, double coeff = 1)
 	}
 	
 }
-void bubble(vector <int> vc, int size, double coeff = 1)
+void bubble(vector <int> vc, int size, int speed_coeff=50, double coeff = 1 )
 {
 	sf::RenderWindow wind(sf::VideoMode(800*coeff, 600), "Title");
 	sf::Event ev;
@@ -339,7 +340,7 @@ void bubble(vector <int> vc, int size, double coeff = 1)
 						if (check != 1)
 						{
 							wind.display();
-							this_thread::sleep_for(chrono::milliseconds(50));
+							this_thread::sleep_for(chrono::milliseconds(speed_coeff));
 
 						}
 
@@ -352,8 +353,12 @@ void bubble(vector <int> vc, int size, double coeff = 1)
 
 void menu()
 {
-
-	int count = 0, size, type;
+	ifstream speed_in;
+	ofstream speed_out;
+	int count = 0, size;
+	int speed_coeff;
+	
+	char type;
 	cout << "Enter the size" << endl;
 	cout << "\nEnter: ";
 	cin >> size;
@@ -374,23 +379,66 @@ void menu()
 		double size1 = double(size);
 		coeff += ((size1 * 100 / 30) - 100) / 100;
 	}
-	cout << "\nChooice the sort's type:\n";
-	cout << "1.Bubble\n2.Insertion\n3.Quick\n\nEnter: ";
-	cin >> type;
-	switch (type)
+	int check = 1,check1=1;
+	while (check1!=0)
 	{
-	case Sbubble:
-		bubble(vc, size,coeff);
-		break;
-	case Sinsertion:
-		insertion(vc, size,coeff);
-		break;
-	case Sshaker:
-		shaker(vc, size,coeff);
-		break;
-	default:
-		break;
+		cout << "\nChooice the sort's type:\n";
+		cout << "1.Bubble\n2.Insertion\n3.Quick\nS.Change the speed\nE.Exit\nEnter: ";
+		cin >> type;
+		
+		speed_in.open("Speed.txt");
+		speed_in >> speed_coeff;
+		speed_in.close();
+		switch (type)
+		{
+		case '1':
+			bubble(vc, size, speed_coeff, coeff);
+			break;
+		case '2':
+			insertion(vc, size, speed_coeff, coeff);
+			break;
+		case '3':
+			shaker(vc, size, speed_coeff, coeff);
+			break;
+		case 'S':
+			while (check != 0)
+			{
+				cout << "\n1.Look the current speed\n2.Enter new speed\n3.Reset to default\nE.Exit\nEnter: ";
+				cin >> type;
+				switch (type)
+				{
+				case '1':
+										
+					cout << "\nCurrent speed = " << speed_coeff << endl;
+					break;
+				case'2':
+
+					cout << "\nEnter the new speed:\n";
+					cin >> speed_coeff;
+					speed_out.open("Speed.txt");
+					speed_out << speed_coeff;
+					speed_out.close();
+					break;
+				case '3':
+					speed_coeff = 50;
+					speed_out.open("Speed.txt");
+					speed_out << speed_coeff;
+					speed_out.close();
+					break;
+				default:
+					check = 0;
+					break;
+				}
+				
+			}
+			check = 1;
+			break;
+		default:
+			check1 = 0;
+			break;
+		}
 	}
+	
 	
 }
 int main()
